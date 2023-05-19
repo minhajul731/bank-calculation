f5d5a091bcc8998f46946764b4bf53d11b68dee4
showkhin1997 <showkhin15@gmail.com>
2023-05-19 13:32:41 +0600

    login form validation

---
+++ js/banking.js
@@ -0,0 +1,19 @@
+document.getElementById('submit-button').addEventListener('click', function () {
+    // email field value
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // password field value
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if (userEmail == 'user@gmail.com' && userPassword == 'user') {
+        window.location.href = 'bank.html';
+    }
+    else {
+        const errorMessage = document.getElementById('error-message');
+        errorMessage.innerText = 'Your UserID invalid';
+    }
+    // email and password field reset
+    emailField.value = '';
+    passwordField.value = '';
+})

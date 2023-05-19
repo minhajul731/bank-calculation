2a1f3223db6f39cc9354df9e7fe638e216ba04e6
showkhin1997 <showkhin15@gmail.com>
2023-05-19 19:19:20 +0600

    update deposite and add balance

--- js/bank.js
+++ js/bank.js
@@ -2,11 +2,22 @@
 document.getElementById('deposite-button').addEventListener('click', function () {
     // get deposited ammount
     const depositeField = document.getElementById('deposite-field');
-    const depositeAmmount = depositeField.value;
+    const inputDepositeAmmountText = depositeField.value;
+    const inputDepositeAmmount = parseFloat(inputDepositeAmmountText);
 
-    // set deposite ammount
+    // set deposite total
     const depositeTotal = document.getElementById('deposit-total');
-    depositeTotal.innerText = depositeAmmount;
+    const previousDepositeTotalText = depositeTotal.innerText;
+    const previousDepositeTotal = parseFloat(previousDepositeTotalText);
+    const updatedDepositeTotal = previousDepositeTotal + inputDepositeAmmount;
+    depositeTotal.innerText = updatedDepositeTotal;
+
+    // update account balance total
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
+    const updateBalanceTotal = previousBalanceTotal + inputDepositeAmmount;
+    balanceTotal.innerText = updateBalanceTotal;
 
     // reset the deposite input field
     depositeField.value = '';
